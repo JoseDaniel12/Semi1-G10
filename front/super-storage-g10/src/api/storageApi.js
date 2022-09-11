35a9f207dc4fcb02d4f86b528880441e2ad306f0
JeffMendez <mjeffryemanuel@gmail.com>
2022-09-10 18:15:59 -0600

    enseñar imagen si es en la card

--- front/super-storage-g10/src/api/storageApi.js
+++ front/super-storage-g10/src/api/storageApi.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+
 const storageApi = axios.create({
-    baseURL: 'http://localhost:9000/'
+    baseURL: 'http://balancer-proyecto1-72833247.us-east-1.elb.amazonaws.com/'
 });
 
 export default storageApi;
